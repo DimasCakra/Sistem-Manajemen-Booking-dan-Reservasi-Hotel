2f293bd4ed4824b4487f0dd6cb378f7fe0eb758f
Damar Widi Nugroho <damarwidhinugroho@gmail.com>
2026-04-05 19:44:10 +0700

    mengubah login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,45 +1,39 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    
-</head>
-<body>
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5hirGcGYW4VJKa63FFemb3xfb23CdjNJlg&s" 
-    class="mx-auto h-30 w-auto" alt="Deskripsi">
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black">Sign in to your account</h2>
-  </div>
+    <title>Login Page</title>
+<link rel="stylesheet" href="{{ asset('css/login.css') }}"><body>
 
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form action="#" method="POST" class="space-y-6">
-      <div>
-        <label for="email" class="block text-sm/6 font-medium text-black-100">Email address</label>
-        <div class="mt-2">
-          <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-black/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
-        </div>
-      </div>
+<div class="login-container">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE5hirGcGYW4VJKa63FFemb3xfb23CdjNJlg&s" alt="Logo">
+    <div class="login-content">
+        <form action="{{ url('/home') }}" method="GET">
+          <h2>Login to Your Account</h2>
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input id="email" type="email" name="email" required autocomplete="email">
+            </div>
 
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-black-100">Password</label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
-          </div>
-        </div>
-        <div class="mt-2">
-          <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-black/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
-        </div>
-      </div>
-
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
-      </div>
-    </form>
-  </div>
+            <div class="form-group">
+                <div class="label-wrapper">
+                    <label for="password">Password</label>
+                </div>
+                <input id="password" type="password" name="password" required autocomplete="current-password">
+            </div>
+            <div class="form-group">
+                <div class="checkbox-wrapper">
+                    <input id="remember" type="checkbox" name="remember">
+                    <label for="remember">Remember me</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn-submit">Login</button>
+            </div>
+        </form>
+    </div>
 </div>
+
 </body>
 </html>
