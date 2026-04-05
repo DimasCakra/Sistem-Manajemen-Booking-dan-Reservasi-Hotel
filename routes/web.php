20f10dae3f29c0bddf92ba616ed65a02061e2ce7
Damar Widi Nugroho <damarwidhinugroho@gmail.com>
2026-04-05 19:44:32 +0700

    menyesuaikan agar tombol login bisa pindah ke home

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,12 @@ use App\Http\Controllers\HomeController;
 
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/home', function () {
+    return view('home');
+});
+
 Route::get('/home', [HomeController::class, 'index']);
