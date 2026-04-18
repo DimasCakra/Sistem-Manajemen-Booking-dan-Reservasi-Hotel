a22d3eb683ee8b85eca6d3b80a7dc31dfd381ac8
Dimas Cakra <nemocat09@gmail.com>
2026-04-18 20:41:43 +0700

    new database; menambah database data dummy untuk testing halaman

--- database/migrations/2026_04_18_074445_create_reservations_table.php
+++ database/migrations/2026_04_18_074445_create_reservations_table.php
@@ -6,16 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('reservations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up(): void
+{
+    Schema::create('reservations', function (Blueprint $table) {
+        $table->id();
+        $table->string('room_type');
+        $table->string('room_number');
+        $table->string('nama_lengkap');
+        $table->string('whatsapp');
+        $table->string('email');
+        $table->string('jumlah_tamu');
+        $table->string('check_in_out');
+        $table->string('status')->default('ongoing'); // ongoing, done, refund
+        $table->integer('total_biaya');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
