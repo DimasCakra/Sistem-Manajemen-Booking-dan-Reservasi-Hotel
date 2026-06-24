05c7aead7f6c715c401491d59c3f6f0cadaccf43
Dimas Cakra <nemocat09@gmail.com>
2026-06-24 08:24:51 +0700

    memperbaiki halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,12 +82,12 @@
                 <form action="{{ url('/katalog') }}" method="GET" class="space-y-6">
                     <div class="form-group">
                         <label class="block text-xs font-bold text-gray-400 uppercase mb-3 ml-1 tracking-wider">Tanggal Check-in</label>
-                        <input type="date" id="checkin" name="checkin" value="{{ $checkin }}" required class="w-full p-4 bg-gray-50 border border-gray-200 rounded-xl outline-none focus:ring-2 focus:ring-[#254117]">
+                        <input type="date" id="checkin" name="checkin" value="{{ request('checkin', now()->toDateString()) }}" class="w-full p-4 bg-gray-50 border border-gray-200 rounded-xl outline-none focus:ring-2 focus:ring-[#254117]">
                     </div>
 
                     <div class="form-group">
                         <label class="block text-xs font-bold text-gray-400 uppercase mb-3 ml-1 tracking-wider">Tanggal Check-out</label>
-                        <input type="date" id="checkout" name="checkout" value="{{ $checkout }}" required class="w-full p-4 bg-gray-50 border border-gray-200 rounded-xl outline-none focus:ring-2 focus:ring-[#254117]">
+                        <input type="date" id="checkout" name="checkout" value="{{ request('checkout', now()->addDay()->toDateString()) }}" required class="w-full p-4 bg-gray-50 border border-gray-200 rounded-xl outline-none focus:ring-2 focus:ring-[#254117]">
                     </div>
 
                     <div class="form-group">
@@ -143,7 +143,7 @@
 
             <div class = "flex items-center gap-5 px-10 py-4 md:py-0">
                 <div class="p-3 rounded-2xl bg-[#C4922A]/10 text-[#C4922A] shrink-0 border border-[#C4922A]/20">
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor">
+                    <svg class="w-6 h-6" xmlns="http://wwx  w.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
                     </svg>
                 </div>
